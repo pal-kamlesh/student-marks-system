57c6ce7e5f62dd99f4092e34c122f0e16035dafc
pal-kamlesh <kamleshiit5@gmail.com>
2025-09-26 11:18:39 +0530

    change db url

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
